5bfdec63e12f290240e4f0a15893befc23089d95
spokeinfinity <czelukpro@gmail.com>
2021-10-28 10:14:46 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/quiz/Question.java
@@ -0,0 +1,16 @@
+package com.example.quiz;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer){
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+}
